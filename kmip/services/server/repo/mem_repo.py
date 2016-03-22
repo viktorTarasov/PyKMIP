bcfcf5b27082a4cda164d28b50fb5a7331e48c4d
Viktor Tarasov <viktor.tarasov@gmail.com>
2016-03-22 18:58:21 +0100

    vta: services: mem-repo: locate

--- kmip/services/server/repo/mem_repo.py
+++ kmip/services/server/repo/mem_repo.py
@@ -14,7 +14,11 @@
 # under the License.
 
 from kmip.services.server.repo.repo import ManagedObjectRepo
-
+from kmip.core.attributes import UniqueIdentifier
+from kmip.core.enums import AttributeType
+from kmip.core.enums import ObjectGroupMember
+from kmip.core.enums import StorageStatusMask
+from kmip.core.objects import Attribute
 
 class MemRepo(ManagedObjectRepo):
 
@@ -27,6 +31,7 @@ class MemRepo(ManagedObjectRepo):
         uuid = "{0}".format(self.uuid)
         self.repo[uuid] = (managed_object, attributes)
         self.uuid += 1
+
         return uuid
 
     def get(self, uuid):
@@ -48,4 +53,51 @@ class MemRepo(ManagedObjectRepo):
 
     def locate(self, maximum_items, storage_status_mask,
                object_group_member, attributes):
-        raise NotImplementedError
+        result = list()
+
+        if maximum_items != None and  maximum_items.value == 0:
+                return result
+
+        for idx in self.repo:
+            (managed_object, obj_attributes) = self.repo[idx]
+
+            matched_attrs = 0
+            fresh = False
+            have_archive_date = False
+
+            for attr in obj_attributes:
+                for lattr in attributes:
+                    if (attr.attribute_name == lattr.attribute_name
+                            and attr.attribute_value == lattr.attribute_value) :
+                        matched_attrs += 1
+
+                if attr.attribute_name.value == AttributeType.FRESH.value:
+                    fresh = attr.attribute_value
+
+                if attr.attribute_name.value == AttributeType.ARCHIVE_DATE.value:
+                    have_archive_date = True
+
+            if object_group_member != None:
+                if object_group_member.value == ObjectGroupMember.GROUP_MEMBER_FRESH:
+                     if not fresh :
+                         continue
+                else:
+                     if fresh :
+                         continue
+
+            if storage_status_mask != None:
+                if storage_status_mask.value == StorageStatusMask.ARCHIVAL_STORAGE:
+                    if not have_archive_date:
+                        continue
+                if storage_status_mask.value == StorageStatusMask.ONLINE_STORAGE:
+                    if have_archive_date:
+                        continue
+
+            if matched_attrs == len(attributes):
+                result.append(UniqueIdentifier(idx))
+
+            if maximum_items != None and maximum_items.value <= len(result):
+                break
+
+        return result
+
